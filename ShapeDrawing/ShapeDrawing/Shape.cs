7d76d338e0d02d13efb360dc18f595563483f701
Wessel Gordijn <wesselsimon@hotmail.com>
2017-10-23 20:21:49 +0200

    Redundancy deleted

--- ShapeDrawing/ShapeDrawing/Shape.cs
+++ ShapeDrawing/ShapeDrawing/Shape.cs
@@ -4,16 +4,7 @@ using System.Drawing;
 
 public abstract class Shape
 {
-
-    protected Output output;
-	public Shape()
-	{
-	}
-
     public virtual void Draw(Output output, Graphics Canvas = null)
     {
-
     }
-        
-	
 }
